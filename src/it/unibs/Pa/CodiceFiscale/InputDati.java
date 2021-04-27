1957becf76be091b5c2004003b5c1e98d942e6bb
d-martinelli1612 <d.martinelli1612@gmail.com>
2021-04-27 20:44:00 +0200

    Inizializzazione lettore
    Il lettore del programma viene correttamente inizializzato

--- src/it/unibs/Pa/CodiceFiscale/InputDati.java
+++ src/it/unibs/Pa/CodiceFiscale/InputDati.java
@@ -1,4 +1,23 @@
 package it.unibs.Pa.CodiceFiscale;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+
 public class InputDati {
+
+    private void LeggiXML() {
+        XMLInputFactory xmlif = null;
+        XMLStreamReader xmlr = null;
+
+        try{
+            xmlif = XMLInputFactory.newInstance();
+            xmlr = xmlif.createXMLStreamReader("codiciFiscali", new FileInputStream("codiciFiscali"));
+        } catch (Exception e){
+            System.out.println("Errore nell'inzializzazione del reader");
+            System.out.println(e.getMessage());
+        }
+
+
+    }
 }
