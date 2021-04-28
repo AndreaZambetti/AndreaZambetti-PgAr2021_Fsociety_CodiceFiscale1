2c834970fb2b052ef0d0de713a5fbd5a779792be
d-martinelli1612 <d.martinelli1612@gmail.com>
2021-04-28 09:38:35 +0200

    Codice di lettura funzionante

--- src/it/unibs/Pa/CodiceFiscale/InputDati.java
+++ src/it/unibs/Pa/CodiceFiscale/InputDati.java
@@ -13,10 +13,13 @@ public class InputDati {
     {
         XMLInputFactory xmlif = null;
         XMLStreamReader xmlr = null;
+        String codiciFiscali = "codiciFiscali.xml";
+        String comuni = "comuni.xml";
+        String inputPersone = "inputPersone.xml";
 
         try {
             xmlif = XMLInputFactory.newInstance();
-            xmlr = xmlif.createXMLStreamReader("codiciFiscali", new FileInputStream("codiciFiscali"));
+            xmlr = xmlif.createXMLStreamReader(inputPersone, new FileInputStream(inputPersone));
         } catch (Exception e) {
             System.out.println("Errore nell'inzializzazione del reader");
             System.out.println(e.getMessage());
@@ -25,25 +28,28 @@ public class InputDati {
         //l'istruzione nel while dava errore: add exception to method signatur
         //Ho usato la correzione automatica del sistema cliccando sull'errore
         while (xmlr.hasNext()) {
+
             switch (xmlr.getEventType()) {
                 case XMLStreamConstants.START_DOCUMENT:
-                    System.out.println("Start Read Doc " + "codiciFiscali");
-                break;
+                    System.out.println("Start Read Doc " + inputPersone);
+                    break;
                 case XMLStreamConstants.START_ELEMENT:
-                    System.out.println("Tag" + xmlr.getLocalName());
-                    for (int i = 0; i < xmlr.getAttributeCount(); i++)
-                        System.out.println(xmlr.getAttributeLocalName(i)/*, xmlr.getAttributeValue(i)*/);
-                break;
+                    System.out.println("Tag " + xmlr.getLocalName());
+                    for (int i = 0; i < xmlr.getAttributeCount(); i++) {
+                        System.out.print(xmlr.getAttributeLocalName(i));
+                        System.out.println(xmlr.getAttributeValue(i));
+                    }
+                    break;
                 case XMLStreamConstants.END_ELEMENT:
                     System.out.println("END-Tag" + xmlr.getLocalName());
-                break;
+                    break;
                 case XMLStreamConstants.COMMENT:
                     System.out.println("//commento " + xmlr.getText());
-                break;
+                    break;
                 case XMLStreamConstants.CHARACTERS:
                     if (xmlr.getText().trim().length() > 0)
                         System.out.println("-> " + xmlr.getText());
-                break;
+                    break;
             }
             xmlr.next();
         }
